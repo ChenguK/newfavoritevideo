c8415c8b998b1319d7e011ba023251265f0a03d7
ChenguK <ChenguKargbo@gmail.com>
2020-07-15 12:25:53 -0400

    fixed modal button look'
    ~

--- js/script.js
+++ js/script.js
@@ -10,17 +10,13 @@ const $video = $('.modal-content .video-container');
 const $input = $("input[type='text']");
 const $modal = $('.modal');
 
-$ul.on('click', 'img', handleClick);
 
-$modal.modal();
-    const instance = M.Modal.getInstance($modal);
 
 function handleClick(evt) {
         getVideos(evt.target.dataset.url, true); 
     }
 
 $("form").on("submit", getVideos);
-event
 function getVideos(evt) {
     event.preventDefault();
     userInput = $input.val();
@@ -41,29 +37,81 @@ function getVideos(evt) {
 function generateHTml() { 
     return videoData.map(function(v) {
         return `
-        <li class="collection-item"><div><b class="grey-darken-text">${v.snippet.title}</b><a href="#myModal" role="button" data-toggle="modal" class="secondary-content blue-text"><i class="material-icons"><img src=${v.snippet.thumbnails.medium.url} "data-target="modal1" class="btn modal-trigger" alt="video thumbnail"></i></a><br>${v.snippet.channelTitle}
+        <li class="collection-item"><div><b class="blue-grey-text">${v.snippet.title}</b><a href="#" class="secondary-content blue-text"><i class="material-icons"><img class="modal-trigger" data-target="modal1" src=${v.snippet.thumbnails.medium.url} alt="video thumbnail"></i></a><br>${v.snippet.channelTitle}
         </div>
         </li>`
     })
 }
-// <a href="#!" class="modal-close waves-effect waves-green btn-flat">Close</a>
-// 'data-target="modal1" class="btn modal-trigger'
+
+
 
 function render() {
     const html = generateHTml().join("");
     $('h4').html('Results for ' + userInput )
     $ul.html(html);
-    // for(let i = 0; i < videoData.items.length; i++) { 
+    // $modal.on("click", generateModal().join(""));
     $title.html(videoData.snippet.title);
-        // console.log(videoData.items[i].snippet.title);
     $channel.html(videoData.snippet.channelTitle);
     $description.html(videoData.snippet.description);
-    // $img.attr('src', videoData.thumbnails.medium.url); 
-    $video.attr(`https://www.youtube.com/watch?v=` + videoData.id.videoId);
-    instance.open();
+    $video.attr(videoData.id + `https://www.youtube.com/watch?v=` + videoId);
+    $(".modal").modal();
+    // const video = generateModal().join("");
+    // $modal.video(video)
+    // $title.video(videoData.snippet.title);
+    // $channel.video(videoData.snippet.channelTitle);
+    // $description.html(videoData.snippet.description);
+    // $video.attr(videoData.id + `https://www.youtube.com/watch?v=` + videoId);
+    // instance.open();
 }
 
 function submitForm() {
     $('form[name="contact-form"]').submit();
-    $('input[type="text"], textarea').val("");
+    $('input[type="text"], textarea').reset("");
   }
+
+//   $ul.on('click', 'modal', handleModal);
+
+function handleModal(evt) {
+    $modal(evt.target.dataset.url, true);
+}
+$img.on("click", generateModal);
+
+
+//   function generateModal(evt) {
+//     $modal.modal();
+//     const instance = M.Modal.getInstance($modal);
+//   }
+
+// $(".modal-trigger").on("click", generateModal)
+// $(document).ready(function(){
+    
+//     )};
+
+
+
+
+//     event.preventDefault();
+//     userInput = $input.val();
+//     $.ajax({
+// 		url:baseUrl  + userInput + `&type=video&key=${config.API_KEY}` 
+//     }).then(
+//         (data)=> { 
+//             videoData = data.items;
+//             // console.log("DATA IS ", data.items)
+// 			Modal.render();
+//         }, 
+//         (error) => { 
+
+//             console.log("ERROR IS ", error)
+//         })
+// }
+
+// function Modal.render() {
+
+// }
+
+// // document.addEventListener("DOMContentLoaded", function(){
+// //     const box = document.querySelectorAll(".modal");
+// //     M.Modal.init(box,{});
+
+// // })
