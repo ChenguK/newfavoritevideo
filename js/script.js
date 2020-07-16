0e5d8a7c0446cb971ffd8e20bebd4836e566d4ce
ChenguK <ChenguKargbo@gmail.com>
2020-07-16 17:00:09 -0400

    created modal link

--- js/script.js
+++ js/script.js
@@ -6,19 +6,28 @@ const $title = $('#title');
 const $channel = $('#channel');
 const $description = $('#description');
 const $img = $('img');
-const $video = $('.modal-content .video-container');
+// const $video = $('.modal-content .video-container');
 const $input = $("input[type='text']");
 const $modal = $('.modal');
+const $modalimg = $('.modal-content .modalimg');
+const $imglink = $('#imglink');
+
 $(document).ready(function(){
       $modal.modal();  
      instance = M.Modal.getInstance($modal);
     });
 
+
+    
+    // $modal.on("click", modalimg, function(evt){
+        
+    // })
+
 function handleClick(evt) {
         getVideos(evt.target.dataset.url, true); 
     }
+
 $ul.on("click", "img", function(evt){
-    // instance.open();
     handleOpenModal(evt.target.src);
     render();
 })
@@ -28,11 +37,11 @@ function getVideos(evt) {
     event.preventDefault();
     userInput = $input.val();
     $.ajax({
-		url:baseUrl  + userInput + `&type=video&key=${config.API_KEY}` 
+        url:baseUrl  + userInput + `&type=video&key=${config.API_KEY}`,
     }).then(
         (data)=> { 
             videoData = data.items;
-            console.log("DATA IS ", data.items)
+            console.log(data);
 			render();
         }, 
         (error) => { 
@@ -44,31 +53,39 @@ function getVideos(evt) {
 function generateHTml() { 
     return videoData.map(function(v) {
         return `
-        <li class="collection-item"><div><b class="blue-grey-text">${v.snippet.title}</b><div class="secondary content"><img src=${v.snippet.thumbnails.medium.url} alt="video thumbnail"></i><br>${v.snippet.channelTitle}
+        <li class="collection-item"><div><b class="blue-grey-text">${v.snippet.title}</b><div class="secondary content"><img class="thumbnail" src=${v.snippet.thumbnails.medium.url} alt="video thumbnail"></i><br>${v.snippet.channelTitle}
         </div>
         </li>`
     })
 }
 
-
-
 function render() {
     const html = generateHTml().join("");
-    $('h5').html('Results for ' + userInput )
+    $('h3').html(`Results for${userInput}`);
     $ul.html(html);
-   
 }
 
 function handleOpenModal(imgsrc){
    const videoObject = videoData.find(function(video){
+
         return video.snippet.thumbnails.medium.url === imgsrc;
     });
-    
+
+    $(document).ready(function(){
+        $modalimg.find(function() {
+            $imglink.click(function(){
+                open('https://www.youtube.com/watch?v=' + videoObject.id.videoId + 'target=_blank');
+    })
+ })
+})
+
 
     $title.html(videoObject.snippet.title);
     $channel.html(videoObject.snippet.channelTitle);
     $description.html(videoObject.snippet.description);
-    $video.attr("src", `"https://www.youtube.com/embed/` + videoObject.id.videoId);
+    // $video.attr("src", `"https://www.youtube.com/embed/` + videoObject.id.videoId);
+    $modalimg.attr("src", videoObject.snippet.thumbnails.high.url);
+    $imglink.attr('href', 'https://www.youtube.com/watch?v=' + videoObject.id.videoId + 'id=redirect' + 'target=_blank');
     instance.open();
 }
 
