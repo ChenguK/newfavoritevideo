b3e9bb90253af4604a961328135a04c2dc532fb7
ChenguK <ChenguKargbo@gmail.com>
2020-07-16 22:02:44 -0400

    trying to hide in plain site

---
+++ js/config.js
@@ -0,0 +1,6 @@
+const config = {
+    // API_KEY: "AIzaSyCAOEEftjVt2as6ECebiGCGIn-gLTvXTxE",
+    // API_KEY1: "AIzaSyB1j2ykvImzfbUnbTGF7sSoECSyTEJTKyk",
+    // API_KEY: "AIzaSyCbhe1BagxenYpRqOA6LtLNYUyvaiN2cf4",
+    API_KEY: 'AIzaSyCHtU2bkT6e6KUx-Me4is3mnriJlqkEWRU',
+};
